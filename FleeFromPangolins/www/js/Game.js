d01385123c46ca2b9adca5a497818aa8367573d7
laidet_r <rodolphe.laidet@epitech.eu>
2016-07-11 13:03:25 +0200

    Création du fichier Game.js, qui apparement va contenir le code principal du jeu.

---
+++ FleeFromPangolins/www/js/Game.js
@@ -0,0 +1,27 @@
+/*jslint browser:true this*/
+/*global window console Phaser*/
+
+(function (global) {
+    "use strict";
+
+    var FFP = global.FFP || {};
+
+    FFP.Game = function () {
+        return;
+    };
+
+    FFP.Game.prototype.preload = function () {
+    };
+
+    FFP.Game.prototype.create = function () {
+    };
+
+    FFP.Game.prototype.update = function () {
+    };
+
+    FFP.Game.prototype.render = function () {
+    };
+
+    global.FFP = FFP;
+
+}(window));
