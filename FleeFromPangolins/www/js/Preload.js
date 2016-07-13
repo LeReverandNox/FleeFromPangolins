a8f1fa80e697d97874e0ebbe23358195c5d321ce
laidet_r <rodolphe.laidet@epitech.eu>
2016-07-13 18:08:15 +0200

    Rajout de la spritesheet de la ghoul dans le preload ainsi que son png

--- FleeFromPangolins/www/js/Preload.js
+++ FleeFromPangolins/www/js/Preload.js
@@ -19,6 +19,7 @@
 
         this.load.spritesheet('simon', 'assets/images/simon.png', 29, 47, 7);
         this.load.spritesheet('dracula', 'assets/images/dracula.png', 88, 81, 3);
+        this.load.spritesheet('ghoul', 'assets/images/ghoul.png', 28, 55, 2);
         this.load.image('ground', 'assets/images/ground.png');
         this.load.image('grass', 'assets/images/grass.png');
     };
