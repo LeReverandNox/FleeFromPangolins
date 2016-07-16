dc2b47d64b6d0ccab8a242341498ca73e66195c4
laidet_r <rodolphe.laidet@epitech.eu>
2016-07-16 20:59:16 +0200

    On charge l'asset de ball dans le Preload

--- FleeFromPangolins/www/js/Preload.js
+++ FleeFromPangolins/www/js/Preload.js
@@ -22,6 +22,7 @@
         this.load.spritesheet('ghoul', 'assets/images/ghoul.png', 28, 55, 2);
         this.load.image('ground', 'assets/images/ground.png');
         this.load.image('grass', 'assets/images/grass.png');
+        this.load.image('ball', 'assets/images/ball.png');
         this.load.audio('main-theme', 'assets/sounds/main-theme.mp3');
     };
 
